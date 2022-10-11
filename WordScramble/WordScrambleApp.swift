3719f363b4448020563da3908f95d9292ed2c513
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-10-10 20:24:03 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kevin Mattocks on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
